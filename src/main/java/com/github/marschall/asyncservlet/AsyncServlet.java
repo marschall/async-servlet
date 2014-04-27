d9b705cbe59063bd4215e74b56926d11263e3673
Philippe Marschall <philippe.marschall@gmail.com>
2014-04-27 14:08:55 +0200

    async work

--- src/main/java/com/github/marschall/asyncservlet/AsyncServlet.java
+++ src/main/java/com/github/marschall/asyncservlet/AsyncServlet.java
@@ -1,7 +1,11 @@
 package com.github.marschall.asyncservlet;
 
 import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.util.concurrent.CountDownLatch;
+import java.util.logging.Logger;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,10 +15,30 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "asyncServlet", urlPatterns= "/asyncServlet", asyncSupported = true)
 public class AsyncServlet extends HttpServlet {
   
+  private static final Logger LOG = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
+  
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     resp.setContentType("text/plain");
-    resp.getWriter().print("OK");
+    CountDownLatch latch = new CountDownLatch(1);
+    
+    
+    AsyncContext context = req.startAsync();
+    LOG.info("sync work in" + Thread.currentThread().getName());
+    context.start(() -> {
+      LOG.info("async work in" + Thread.currentThread().getName());
+      try {
+        latch.await();
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        LOG.warning("interrupted");
+      }
+    });
+    LOG.info("count down");
+    latch.countDown();
+    
+    context.getResponse().getWriter().print("OK");
+    context.complete();
   }
   
 
